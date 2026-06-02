9d2a230c034cc0f0f96db1f3ec0c12d8503f4278
Pavani kasul <pavanikasul244@gmail.com>
2026-06-02 15:06:31 +0530

    Update page.tsx

--- src/app/admin/dashboard/registrations/page.tsx
+++ src/app/admin/dashboard/registrations/page.tsx
@@ -587,7 +587,7 @@ export default function RegistrationsPage() {
                         className={styles.btnPrimary} 
                         style={{ padding: '0.4rem 0.8rem', background: '#22c55e', color: 'white', fontSize: '0.85rem', fontWeight: 'bold', border: 'none', borderRadius: '6px', cursor: 'pointer' }} 
                         onClick={() =>{console.log("ACCEPT BUTTON CLICKED", reg.id);
-                                       handleAccept(reg.id)}
+                                       handleAccept(reg.id)}}
                       >
                         Accept
                       </button>
